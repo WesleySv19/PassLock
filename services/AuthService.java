f62b7d40e037d1966a8b769fd82a86962871d354
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-06-09 23:55:10 -0300

    refactor: some configs

--- services/AuthService.java
+++ services/AuthService.java
@@ -1,14 +1,14 @@
 package services;
 
-import java.io*;
+import java.io.*;
 import java.nio.file.*;
 import java.security.*;
 import java.util.Scanner;
 
-Public class AuthService {
+public class AuthService {
     private static final STRING HASH_FILE = "data/master.hash";
 
-    public void createMasterPassword () throws IOException, NoSuchAlgorithmException {
+    public void createMasterPassword() throws IOException, NoSuchAlgorithmException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Crie uma senha-mestra: ");
         String password = scanner.nextLine();
@@ -17,7 +17,7 @@ Public class AuthService {
         System.out.println("Senha-mestra criada com sucesso!");
     }
 
-    public boolean verifyMasterPassword () throws IOException, NoSuchAlgorithmException {
+    public boolean verifyMasterPassword() throws IOException, NoSuchAlgorithmException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite sua senha-mestra: ");
         String inputPassword = scanner.nextLine();
@@ -25,7 +25,7 @@ Public class AuthService {
         return storedHash.equals(hashPassword(inputPassword));
     }
 
-    private String HashPassword (String password) NoSuchAlgorithmException {
+    private String HashPassword(String password) throws NoSuchAlgorithmException {
         MessageDigest md = MessageDigest.getInstace("SHA-254");
         byte[] hashBytes = md.digest(password.getBytes());
         StringBuilder sb = new StringBuilder();
@@ -33,7 +33,10 @@ Public class AuthService {
         for (byte b : hashBytes) {
             sb.append(String.format("%02x", b));
         }
-
         return sb.toString();
+    }
+
+    public boolean isMasterPasswordSet() {
+        return Files.exists(Paths.get(HASH_FILE));
     }
 }
